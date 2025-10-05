cb2687b429d573a82573d183b612acc4aee95f3a
EmRodop23 <160190271+EmRodop23@users.noreply.github.com>
2025-10-05 12:31:27 -0400

    Create R02_EXP02_J.java
    Non-compliant Code Example: This non-compliant code example uses the Object.equals() method to compare two arrays.

---
+++ R02_EXP02_J.java
@@ -0,0 +1,6 @@
+// Rule 02. Expressions (EXP)
+// EXP02-J. Do Not Use the Object.equals() Method to Compare Two Arrays
+
+int[] arr1 = new int[20]; // Initialized to 0
+int[] arr2 = new int[20]; // Initialized to 0
+System.out.println(arr1.equals(arr2)); // Prints false
