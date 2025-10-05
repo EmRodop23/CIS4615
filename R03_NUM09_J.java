25bf9a09286de49f91638c8cc68454dc298b411a
EmRodop23 <160190271+EmRodop23@users.noreply.github.com>
2025-10-05 12:35:24 -0400

    Update R03_NUM09_J.java
    Compliant Solution
    
    This compliant solution uses an integer loop counter from which the desired floating-point value is derived:

--- R03_NUM09_J.java
+++ R03_NUM09_J.java
@@ -1,6 +1,7 @@
 // Rule 03. Numeric Types and Operations (NUM)
 // NUM09-J. Do Not Use Floating-Point Variables as Loop Counters
 
-for (float x = 0.1f; x <= 1.0f; x += 0.1f) {
+for (int count = 1; count <= 10; count += 1) {
+  float x = count/10.0f;
   System.out.println(x);
 }
